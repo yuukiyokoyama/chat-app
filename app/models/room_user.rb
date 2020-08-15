a57cdf659ce72123808d1bc4ef8e0d5d5ef68d16
yuukiyokoyama <yuukitigars89@gmail.com>
2020-08-15 16:02:53 +0900

    messagesテーブルの作成

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,4 +1,12 @@
-class RoomUser < ApplicationRecord
-  belongs_to :room
-  belongs_to :user
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  has_many :messages
+
+  validates :name, presence: true
 end
