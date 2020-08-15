dfceb8caee292014172a084c73b1bf4e73db282a
yuukiyokoyama <yuukitigars89@gmail.com>
2020-08-15 15:40:56 +0900

    チャット画面の編集

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ApplicationRecord
-<<<<<<< HEAD
+
   has_many :room_users
   has_many :users, through: :room_users
 
-=======
->>>>>>> parent of ffa9fc6... room_usersテーブルの作成
+  validates :name, presence: true
+
 end
